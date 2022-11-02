0dbf41e7217c40467343d0c97b77b4ae5a515944
Hanna <hanna@dotbite.at>
2022-11-02 21:25:57 +0100

    A sail (docker), C setup cmd

---
+++ app/Console/Commands/SetupProject.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class SetupProject extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Setup the project';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call('key:generate');
+        Artisan::call('migrate:fresh');
+        Artisan::call('db:seed');
+
+        $this->info('Project set up successfully.');
+    }
+}
